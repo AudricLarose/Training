d9c0a958a72c76949d753cc0949d809f488dc210
Larose Audric <l.audric@gmail.com>
2020-04-30 18:53:54 +0200

    Finalisation du projet 2

---
+++ P7_Go4Lunch/P7_Larose_Audric/P7_Go4Lunch/P7_Go4Lunch/app/src/main/java/entrainement/timer/p7_go4lunch/Bases/ActivityDetails.java
@@ -0,0 +1,372 @@
+package entrainement.timer.p7_go4lunch.Bases;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import entrainement.timer.p7_go4lunch.DI.DI;
+import entrainement.timer.p7_go4lunch.R;
+import entrainement.timer.p7_go4lunch.utils.ViewModelApi;
+import entrainement.timer.p7_go4lunch.api.collegue.ExtendedServiceCollegue;
+import entrainement.timer.p7_go4lunch.api.restaurant.ExtendedServicePlace;
+import entrainement.timer.p7_go4lunch.model.Collegue;
+import entrainement.timer.p7_go4lunch.model.Me;
+import entrainement.timer.p7_go4lunch.model.Results;
+import entrainement.timer.p7_go4lunch.utils.Other;
+import entrainement.timer.p7_go4lunch.utils.collegue.AdaptateurQuiVient;
+
+import static android.Manifest.permission.CALL_PHONE;
+
+public class ActivityDetails extends AppCompatActivity {
+
+    @BindView(R.id.nomGrand)
+    TextView nom;
+    @BindView(R.id.adresseGrand)
+    TextView adresse;
+    @BindView(R.id.wesiteGrand)
+    CardView internet;
+    @BindView(R.id.likeGrand)
+    CardView like;
+    @BindView(R.id.likenotGrand)
+    CardView unlikebutton;
+    @BindView(R.id.checknotGrand)
+    RelativeLayout put_me_Out;
+    @BindView(R.id.imageGrand)
+    ImageView image;
+    @BindView(R.id.callGrand)
+    CardView tel;
+    @BindView(R.id.nocollegue)
+    CardView noplace;
+    @BindView(R.id.nointernet)
+    ImageView nointernet;
+    @BindView(R.id.backbutton)
+    ImageButton backButton;
+
+
+    private CoordinatorLayout coordinatorLayout;
+
+    private ExtendedServiceCollegue serviceCollegue = DI.getService();
+    private ExtendedServicePlace servicePlace = DI.getServicePlace();
+    private List<String> myLikes = new ArrayList<>();
+    private List<Collegue> collegueList = new ArrayList<>();
+    private String starData;
+    private ViewModelApi viewModelApi;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        coordinatorLayout = findViewById(R.id.coordinate2);
+        RelativeLayout myChoice = findViewById(R.id.checkGrand);
+        RatingBar stars = findViewById(R.id.ratingdetails);
+        
+        ButterKnife.bind(this);
+        myLikes = Me.getMyLikes();
+        viewModelApi = new ViewModelProvider(ActivityDetails.this).get(ViewModelApi.class);
+        AdaptateurQuiVient adapter = new AdaptateurQuiVient(viewModelApi.getwhocome(), this);
+
+        // I verifiy if User have internet and the GPS on.
+        Intent intent = this.getIntent();
+        Bundle extra = intent.getExtras();
+        if (extra != null) {
+            Results place = (Results) extra.getSerializable("Place");
+            if ((place != null) && (place.getPlaceId() != null)) {
+                Other.CallApiOnOnePlacePlease(place.getPlaceId(), new Other.Finish() {
+                    @Override
+                    public void onFinish(Results apiforOnePlaces) {
+                        if (apiforOnePlaces != null && Other.internetIsOn(ActivityDetails.this)) {
+                            initiatePlaceApiDetails(apiforOnePlaces);
+                        } else {
+                            tel.setEnabled(false);
+                            tel.setCardBackgroundColor(getResources().getColor(R.color.quantum_grey));
+                            internet.setEnabled(false);
+                            internet.setCardBackgroundColor(getResources().getColor(R.color.quantum_grey));
+                        }
+                    }
+                });
+                InitiatePlaceNearByElements(myChoice, stars, extra, place);
+                nom.setText(place.getName());
+                collegueList = servicePlace.CompareCollegueNPlace(place, ActivityDetails.this, new ExtendedServicePlace.Increment() {
+                    @Override
+                    public void onFinish() {
+                        if (collegueList.isEmpty() || viewModelApi.getwhocome().getValue().isEmpty()) {
+                            noplace.setVisibility(View.VISIBLE);
+                        } else {
+                            noplace.setVisibility(View.GONE);
+                        }
+                    }
+                });
+                if (Other.internetIsOn(ActivityDetails.this)) {
+                    enableButtons(myChoice, place);
+                } else {
+                    disableButtons(myChoice);
+                }
+                RecyclerView recyclerView = findViewById(R.id.RecycleGrand);
+                recyclerView.bringToFront();
+                recyclerView.setHasFixedSize(true);
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(adapter);
+            }
+        }
+    }
+
+    private void initiatePlaceApiDetails(Results apiforOnePlaces) {
+        setAdressIfPossible(apiforOnePlaces);
+        setPhoneIfPossible(apiforOnePlaces);
+        setWebsiteIfPossible(apiforOnePlaces);
+        setPhotoIfPossible(apiforOnePlaces);
+    }
+
+    private void setAdressIfPossible(Results apifor) {
+        if (apifor.getVicinity()!=null) {
+            adresse.setText(apifor.getVicinity());
+        }
+    }
+
+    private void InitiatePlaceNearByElements(RelativeLayout myChoice, RatingBar stars, Bundle extra, Results place) {
+        setPhotoIfPossibleApi(place);
+        setRatingIfPossibleApi(stars, extra, place);
+        setChoiceIfPossibleApi(myChoice, place);
+        setLikeIfPossibleApi(place);
+    }
+
+    private void setLikeIfPossibleApi(Results place) {
+        if (myLikes != null) {
+            if (myLikes.contains(place.getPlaceId())) {
+                like.setVisibility(View.GONE);
+                unlikebutton.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void setChoiceIfPossibleApi(RelativeLayout myChoice, Results place) {
+        if (Me.getMy_choice() != null) {
+            if (Me.getMy_choice().equals(place.getName())) {
+                myChoice.setVisibility(View.GONE);
+                put_me_Out.setVisibility(View.VISIBLE);
+            } else {
+                myChoice.setVisibility(View.VISIBLE);
+                put_me_Out.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private void setRatingIfPossibleApi(RatingBar stars, Bundle extra, Results place) {
+        if (place.getRating() != null) {
+            starData = extra.getString("etoile");
+            stars.setRating(Float.parseFloat(place.getRating().toString()));
+        } else {
+            place.setRating(Double.valueOf(0));
+        }
+    }
+
+    private void setPhotoIfPossibleApi(Results place) {
+        if (place.getPhotos() != null) {
+            if (place.getPhotos().get(0) != null) {
+                String html = Other.getUrlimage(place.getPhotos().get(0).getPhotoReference(), String.valueOf(place.getPhotos().get(0).getWidth()));
+                Picasso.get().load(html).into(image);
+            }
+        } else if (place.getPhoto() != null) {
+            if (place.getPhoto() != null) {
+                String html = Other.getUrlimage(place.getPhoto(), "300");
+                Picasso.get().load(html).into(image);
+            }
+        }
+    }
+
+    private void setPhotoIfPossible(Results apiforOnePlaces) {
+        if (apiforOnePlaces.getPhotos() != null) {
+            if (apiforOnePlaces.getPhotos().get(0) != null) {
+                String html = Other.getUrlimage(apiforOnePlaces.getPhotos().get(0).getPhotoReference(), String.valueOf(apiforOnePlaces.getPhotos().get(0).getWidth()));
+                Picasso.get().load(html).into(image);
+            }
+        }
+    }
+
+    private void setWebsiteIfPossible(Results apiforOnePlaces) {
+        if (apiforOnePlaces.getWebsite() == null || apiforOnePlaces.getWebsite().equals("null")) {
+            internet.setEnabled(false);
+            internet.setCardBackgroundColor(getResources().getColor(R.color.quantum_grey));
+        } else {
+            internet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(apiforOnePlaces.getWebsite()));
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+    private void setPhoneIfPossible(Results apiforOnePlaces) {
+        if (apiforOnePlaces.getFormattedPhoneNumber() == null) {
+            tel.setEnabled(false);
+            tel.setCardBackgroundColor(getResources().getColor(R.color.quantum_grey));
+        } else {
+            tel.setOnClickListener(new View.OnClickListener() {
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    String phone1 = "tel:" + Objects.requireNonNull(apiforOnePlaces).getFormattedPhoneNumber();
+                    intent.setData(Uri.parse(phone1));
+                    if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(ActivityDetails.this, new String[]{CALL_PHONE}, 1);
+                    } else {
+                        startActivity(intent);
+                    }
+                    Snackbar.make(coordinatorLayout, getString(R.string.call), Snackbar.LENGTH_LONG).show();
+                }
+
+            });
+        }
+    }
+
+    private void enableButtons(RelativeLayout myChoice, Results place) {
+        nointernet.setVisibility(View.GONE);
+        image.setVisibility(View.VISIBLE);
+        iPressLikeButton(place);
+        iPressChoiceButton(myChoice, place);
+        iUnpresschoiceButton(myChoice, place);
+        iUnpressLikeButton(place);
+        IpressBack();
+    }
+
+    private void disableButtons(RelativeLayout myChoice) {
+        like.setEnabled(false);
+        like.setCardBackgroundColor(getResources().getColor(R.color.quantum_grey));
+        tel.setEnabled(false);
+        tel.setCardBackgroundColor(getResources().getColor(R.color.quantum_grey));
+        internet.setEnabled(false);
+        internet.setCardBackgroundColor(getResources().getColor(R.color.quantum_grey));
+        myChoice.setEnabled(false);
+        image.setVisibility(View.INVISIBLE);
+        nointernet.setVisibility(View.VISIBLE);
+    }
+    private void IpressBack(){
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    private void iPressChoiceButton(RelativeLayout myChoice, Results place) {
+        myChoice.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                noplace.setVisibility(View.GONE);
+                collegueList.add(new Collegue(Me.getMyName(), getString(R.string.mychoice), Me.getMyPhoto()));
+                Other.decrement(Me.getMy_choice());
+                Me.setMy_choice(place.getName());
+                if (place.getPhotos() != null) {
+                    serviceCollegue.addMyChoiceToLists(Me.getMyId(), place.getName(), place.getVicinity(), place.getPlaceId(), place.getRating().toString(), place.getPhotos().get(0).getPhotoReference());
+                } else {
+                    serviceCollegue.addMyChoiceToLists(Me.getMyId(), place.getName(), place.getVicinity(), place.getPlaceId(), place.getRating().toString(), " ada");
+
+                }
+                collegueList = servicePlace.CompareCollegueNPlace(place, ActivityDetails.this);
+                serviceCollegue.twentyFourHourLast(ActivityDetails.this, true);
+                servicePlace.saveMyPlace(place);
+                serviceCollegue.whenNotifyme(ActivityDetails.this, true, place.getName());
+                myChoice.setVisibility(View.GONE);
+                put_me_Out.setVisibility(View.VISIBLE);
+                Snackbar.make(coordinatorLayout, getString(R.string.newPlace), Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void iPressLikeButton(Results place) {
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myLikes = new ArrayList<>();
+                myLikes.add(place.getPlaceId());
+                Me.setMyLikes(myLikes);
+                servicePlace.iLike(place);
+                servicePlace.addMyChoice(place.getPlaceId(), false, true);
+                like.setVisibility(View.GONE);
+                unlikebutton.setVisibility(View.VISIBLE);
+                Snackbar.make(coordinatorLayout, getString(R.string.youlike), Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void iUnpressLikeButton(Results place) {
+        unlikebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myLikes.remove(place.getPlaceId());
+                Me.setMyLikes(myLikes);
+                servicePlace.unLike(place);
+                unlikebutton.setVisibility(View.GONE);
+                like.setVisibility(View.VISIBLE);
+                Snackbar.make(coordinatorLayout, getString(R.string.dontlike), Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void iUnpresschoiceButton(RelativeLayout myChoice, Results place) {
+        put_me_Out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                collegueList.remove(new Collegue(Me.getMyName(), getString(R.string.mychoice), Me.getMyPhoto()));
+                Me.setMy_choice(" ");
+                Me.setId_mychoice(" ");
+                serviceCollegue.dontaddMyChoice();
+                collegueList = servicePlace.CompareCollegueNPlace(place, ActivityDetails.this);
+                serviceCollegue.twentyFourHourLast(ActivityDetails.this, true);
+                servicePlace.unsaveMyPlace(place);
+                serviceCollegue.whenNotifyme(ActivityDetails.this, false, place.getName());
+                put_me_Out.setVisibility(View.GONE);
+                myChoice.setVisibility(View.VISIBLE);
+                Snackbar.make(coordinatorLayout, getString(R.string.retired), Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+}
---
+++ P7_Go4Lunch/P7_Larose_Audric/P7_Go4Lunch/P7_Go4Lunch/app/src/main/java/entrainement/timer/p7_go4lunch/utils/restaurant/Fragmentcarte.java
@@ -0,0 +1,249 @@
+package entrainement.timer.p7_go4lunch.utils.restaurant;
+
+import android.Manifest;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.maps.android.SphericalUtil;
+
+import java.util.Arrays;
+import java.util.List;
+
+import entrainement.timer.p7_go4lunch.BuildConfig;
+import entrainement.timer.p7_go4lunch.DI.DI;
+import entrainement.timer.p7_go4lunch.R;
+import entrainement.timer.p7_go4lunch.api.collegue.ExtendedServiceCollegue;
+import entrainement.timer.p7_go4lunch.api.restaurant.ExtendedServicePlace;
+import entrainement.timer.p7_go4lunch.model.Me;
+import entrainement.timer.p7_go4lunch.model.Results;
+import entrainement.timer.p7_go4lunch.utils.Other;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
+public class Fragmentcarte<call> extends Fragment implements OnMapReadyCallback {
+    private static final String TAG = "MapsActivity";
+    int AUTOCOMPLETE_REQUEST_CODE = 1;
+    private GoogleMap mMap;
+    private ExtendedServicePlace extendedServicePlace;
+    private ImageButton localise;
+    private PlacesClient placesClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private FindCurrentPlaceRequest request;
+    private SearchView searchView = null;
+    private ExtendedServiceCollegue serviceCollegue = DI.getService();
+    private ExtendedServicePlace servicePlace = DI.getServicePlace();
+    private RequestQueue queue;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SimpleCursorAdapter mAdapter;
+        setHasOptionsMenu(true);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View rootView = inflater.inflate(R.layout.fragment_fragmentcarte, container, false);
+        localise = rootView.findViewById(R.id.local);
+        extendedServicePlace = DI.getServicePlace();
+        Places.initialize(getContext(), BuildConfig.API_KEY_GMAIL);
+        placesClient = Places.createClient(getContext());
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        serviceCollegue.updateMyLikes();
+        extendedServicePlace = DI.getServicePlace();
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        queue = Volley.newRequestQueue(getContext());
+        List<entrainement.timer.p7_go4lunch.model.Place> placeList = servicePlace.generateSuggestion();
+        return rootView;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onCreateOptionsMenu(Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+
+        inflater.inflate(R.menu.search, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(getContext().SEARCH_SERVICE);
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+            searchView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.TYPES);
+                    Intent intent = new Autocomplete.IntentBuilder(
+                            AutocompleteActivityMode.FULLSCREEN, placeFields)
+                            .build(getContext());
+                    startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+                }
+            });
+            searchView.setOnSearchClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.TYPES, Place.Field.LAT_LNG);
+                    LatLng center = new LatLng(Me.getMy_latitude(), Me.getMy_longitude());
+                    LatLng northSide = SphericalUtil.computeOffset(center, 800, 45);
+                    LatLng southSide = SphericalUtil.computeOffset(center, 800, 225);
+                    RectangularBounds bounds = RectangularBounds.newInstance(
+                            southSide,
+                            northSide);
+                    Intent intent = new Autocomplete.IntentBuilder(
+                            AutocompleteActivityMode.FULLSCREEN, placeFields)
+                            .setCountry("FR")
+                            .setLocationRestriction(bounds)
+                            .setTypeFilter(TypeFilter.ESTABLISHMENT)
+                            .build(getContext());
+                    startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+                }
+            });
+
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Other.addOnePlaceWithInfo(getContext(), mMap, place);
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                Status status = Autocomplete.getStatusFromIntent(data);
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Other.GPSOnVerify(getContext());
+        Places.initialize(getContext(), BuildConfig.API_KEY_GMAIL);
+        Other.checkrealtime(new Other.Adapterinterf() {
+            @Override
+            public void onFinish(List<Results> listePlaceApi) {
+            }
+
+            @Override
+            public void onRequest(List<Results> request) {
+                if (request != null && !request.isEmpty() && request.get(0).getLat() != null) {
+                    for (int i = 0; i < request.size(); i++) {
+                        double latitude = Double.parseDouble(request.get(i).getLat());
+                        double longitude = Double.parseDouble(request.get(i).getLongi());
+                        LatLng latLng = new LatLng(latitude, longitude);
+                        extendedServicePlace.eventPlace(mMap, request.get(i), latLng);
+                    }
+                }
+            }
+        });
+
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.TYPES);
+        request = FindCurrentPlaceRequest.newInstance(placeFields);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        serviceCollegue.getListCollegue();
+        localise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            double latitude = location.getLatitude();
+                            double longitude = location.getLongitude();
+                            LatLng latLng = new LatLng(latitude, longitude);
+                            Log.d(TAG, "coordonnées: " + latLng.toString());
+                            mMap.addMarker(new MarkerOptions().position(latLng).title(getString(R.string.here)).icon(BitmapDescriptorFactory.fromResource(R.drawable.localisation)));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+                        }
+                    }
+                });
+                Other.mylocation(getActivity(), mMap, getContext());
+            }
+        });
+        if (getContext().checkSelfPermission(ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                getContext().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{ACCESS_FINE_LOCATION}, 1);
+        } else {
+            Other.mylocation(getActivity(), mMap, getContext());
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0]
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationManager locationManager = null;
+            LocationListener locationListener = null;
+            if (ContextCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED)
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+
+    }
+
+}
